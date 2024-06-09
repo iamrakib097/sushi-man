2865e44c067f3e2ee62b8a0897dbdcddbb4a8422
Rakibul Islam <iamrakib097@gmail.com>
2024-06-09 21:43:44 +0600

    everything

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// import images as relative image path won't work with vite/vercel.
+import check from '../assets/check.svg'
+import star from '../assets/star.svg'
+import sushi12 from '../assets/sushi-12.png'
+import sushi11 from '../assets/sushi-11.png'
+import sushi10 from '../assets/sushi-10.png'
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// init AOS animation
+AOS.init({
+    duration: 1000,
+    offset: 100,
+});
+
